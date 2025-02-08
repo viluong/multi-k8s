cc47e8fe77d6ed88acf2dd6dcbcb3a8e81a39efb
viluong <viluong1610@gmail.com>
2025-02-08 16:35:27 +0700

    change int to str for port

--- server/app/config.py
+++ server/app/config.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     pg_host: str
     pg_database: str
     pg_password: str
-    pg_port: int
+    pg_port: str
     redis_host: str
-    redis_port: int
+    redis_port: str
 
     class Config:
         env_file = ".env"
